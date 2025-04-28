71e8cef770dffcc8560f807aee997d3ed34e8757
Alejandro <ale.porras.oroz.0807@gmail.com>
2025-04-28 02:04:13 -0600

    First

---
+++ src/main/java/controller/BaseConvertController.java
@@ -0,0 +1,55 @@
+package controller;
+
+public class BaseConvertController
+{
+    @javafx.fxml.FXML
+    private Text txtMessage1;
+    @javafx.fxml.FXML
+    private Pane buttonPane;
+    @javafx.fxml.FXML
+    private RadioButton octalRB;
+    @javafx.fxml.FXML
+    private Text txtM2;
+    @javafx.fxml.FXML
+    private Text txtMessage;
+    @javafx.fxml.FXML
+    private Pane mainPain;
+    @javafx.fxml.FXML
+    private Pane buttonPane1;
+    @javafx.fxml.FXML
+    private RadioButton binaryRB;
+    @javafx.fxml.FXML
+    private Pane buttonPane111;
+    @javafx.fxml.FXML
+    private Pane buttonPane11;
+    @javafx.fxml.FXML
+    private RadioButton hexadecimalRB;
+    @javafx.fxml.FXML
+    private TextArea tfResult;
+    @javafx.fxml.FXML
+    private TextField decimalValueTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void octalOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void hexadecimalOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void convertOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void binaryOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cleanOnAction(ActionEvent actionEvent) {
+    }
+}
